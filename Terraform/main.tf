bd3d5deb0a84f88fdc8f3a36ab2a11b744de6477
arfath <arfath@git.com>
2024-03-05 22:49:49 +0530

    added

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,4 +4,6 @@ provider "aws" {
 resource "aws_instance" "my-inst" {
   ami=var.ami_value
   instance_type = var.instance_type_value
+  key_name = var.key_name_value
+  security_groups = var.security_groups_value
 }
---
+++ Terraform/variable.tf
@@ -0,0 +1,6 @@
+variable "ami_value" {
+    description = "value of ami_id"
+}
+variable "instance_type_value" {
+    description = "value of instance_type_value"
+}
