4c259f0ba4b09c5e60743d3a74563fc901caa1f9
arfath <arfath@git.com>
2024-03-09 21:57:10 +0530

    first_terraform

--- Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-resource "aws_instance" "my-inst" {
-  ami=var.ami_value
-  instance_type = var.instance_type_value
-  key_name = var.key_name_value
-  security_groups = var.security_groups_value
-}
--- Terraform/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "ami_value" {
-    description = "value of ami_id"
-}
-variable "instance_type_value" {
-    description = "value of instance_type_value"
-}
